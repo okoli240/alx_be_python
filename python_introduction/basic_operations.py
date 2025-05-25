49250b102a808fe702816842962affd57e6b6fd1
okoli240 <okoli240@gmail.com>
2025-05-25 14:45:02 +0000

    Fix formatting and wording in basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,9 @@
-    number1 = 10
-    number2 = 5
-    sum_result = number1 + number2
-    diff_result = number1 - number2
-    product_result = number1 * number2
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+diff_result = number1 - number2
+product_result = number1 * number2
 
-    print("Addition of", number1, "and", number2, "is", sum_result)
-    print("Subtraction of", number1, "and", number2, "is", diff_result)
-    print("Multiplication of", number1, "and", number2, "is", product_result)
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", diff_result)
+print("Multiplication of", number1, "and", number2, "is", product_result)
