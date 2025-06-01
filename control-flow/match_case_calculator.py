db8eaca0885b3be678c42b6cebc8be1fa21e778b
okoli240 <okoli240@gmail.com>
2025-06-01 13:58:40 +0000

    modified

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,15 +4,15 @@ operator = input("Choose the operation (+, -, *, /): ")
 
 match operator:
     case "+":
-        print(f"The result is {num1 + num2}")
+        print(f"The result is {num1 + num2}.")
     case "-":
-        print(f"The result is {num1 - num2}")
+        print(f"The result is {num1 - num2}.")
     case "*":
-        print(f"The result is {num1 * num2}")
+        print(f"The result is {num1 * num2}.")
     case "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            print(f"The result is {num1 / num2}")
+            print(f"The result is {num1 / num2}.")
     case _:
         print("Invalid operation.")
