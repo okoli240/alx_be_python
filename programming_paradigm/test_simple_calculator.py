c641e326066d933af8b9d06b7f20f4fc21bae33d
okoli240 <okoli240@gmail.com>
2025-06-15 18:57:29 +0000

    changed the division fnc name

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,15 +14,15 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(5, 10), -5)
 
-    def test_multiplication(self):  # checker expects this name
+    def test_multiplication(self):  # expected by checker
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 2), -4)
 
-    def test_divide(self):  # checker expects this name
+    def test_division(self):  # rename from test_divide
         self.assertEqual(self.calc.divide(10, 2), 5.0)
-        self.assertEqual(self.calc.divide(9, 3), 3.0)
         self.assertEqual(self.calc.divide(7, 2), 3.5)
-        self.assertEqual(self.calc.divide(5, 0), None)  # division by zero
+        self.assertEqual(self.calc.divide(9, 3), 3.0)
+        self.assertEqual(self.calc.divide(5, 0), None)
 
 if __name__ == '__main__':
     unittest.main()
